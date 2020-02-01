b1993042fd44fdfb21d29069752e248a7b804867
VibroAxe <vibs@macrolevel.co.uk>
2020-02-01 14:22:29 +0000

    Switch circleci to machine engine
    Circleci does not recommend using docker engine for docker deployment
    
    Utilise run-tests from lancachenet/test-suite instead of local copy to
    avoid continual updates accross every project

--- run-tests.sh
+++ run-tests.sh
@@ -1,59 +1,9 @@
 #!/bin/bash
-which goss
 
-if [ $? -ne 0 ]; then
-	echo "Please install goss from https://goss.rocks/install"
-	echo "For a quick auto install run the following"
-	echo "curl -fsSL https://goss.rocks/install | sh"
-	exit $?
+if [[ "$@" == *" -- "* ]]; then
+	SD_LOGLEVEL="-e SUPERVISORD_LOGLEVEL=INFO"
+else
+	SD_LOGLEVEL="-- -e SUPERVISORD_LOGLEVEL=INFO"
 fi
 
-docker build --tag lancachenet/ubuntu:goss-test .
-
-
-case $1 in
-  circleci)
-	shift;
-	mkdir -p ./reports/goss
-	if [[ "$1" == "keepimage" ]]; then
-		KEEPIMAGE=true
-		shift
-	fi
-	export GOSS_OPTS="$GOSS_OPTS --format junit"
-	export CONTAINER_LOG_OUTPUT="reports/goss/docker.log"
-	dgoss run -e SUPERVISORD_LOGLEVEL=INFO $@ lancachenet/ubuntu:goss-test > reports/goss/report.xml
-	echo \
-"Container Output: 
-$(cat reports/goss/docker.log)" \
-	> reports/goss/docker.log
-	#store result for exit code
-	RESULT=$?
-	#delete the junk that goss currently outputs :(
-    sed -i '0,/^</d' reports/goss/report.xml
-	#remove invalid system-err outputs from junit output so circleci can read it
-	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
-    ;;
-  *)
-	if [[ "$1" == "keepimage" ]]; then
-		KEEPIMAGE=true
-		shift
-	fi
-	echo $1
-	if [[ "$1" == "showlog" ]]; then
-		echo "Enabling showlog"
-		SHOWLOG=true
-		shift
-		export CONTAINER_LOG_OUTPUT="docker.log"
-	fi
-	dgoss run -e SUPERVISORD_LOGLEVEL=INFO $@ lancachenet/ubuntu:goss-test
-	if [[ "$SHOWLOG" ==  "true" ]]; then
-		echo "Contianer Output:"
-		cat "docker.log"
-		rm "docker.log"
-	fi
-	RESULT=$?
-    ;;
-esac
-[[ "$KEEPIMAGE" == "true" ]] || docker rmi lancachenet/ubuntu:goss-test
-
-exit $RESULT
+curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/ubuntu:goss-test" $@ $SD_LOGLEVEL
