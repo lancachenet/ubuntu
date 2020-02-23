e6d85da6da0eefa3ab715d98451da6439873c625
James Kinsman <VibroAxe@users.noreply.github.com>
2020-02-23 14:21:12 +0000

    Switch to pip supervisord (#8)
    * Switch to pip supervisord
    Ubuntu baseline supervisord is very out of date, we should use the pip stable version instead
    
    * Account for pip installing supervisor into local
    Pip installs to /usr/local/bin not /usr/bin
    
    * Remove supervisor package check
    Goss package only checks for apt packages. Supervisor must be installed for the process check to pass so this package check was largely irrelevant anyway
    
    * Update run tests to store log output for circleci
    * Store the docker image for later use
    
    * CONTAINER_LOG_OUTPUT changes
    CONTAINER_LOG_OUTPUT does not appear to upload if blank, ensure file is present and containers text even if container output is blank!
    Set SUPERVISORD_LOGLEVEL to something sensible so we actually get some
    logging
    
    * Switch circleci to machine engine
    Circleci does not recommend using docker engine for docker deployment
    Utilise run-tests from lancachenet/test-suite instead of local copy to
    avoid continual updates accross every project
    
    * Update to python 3

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,13 @@ MAINTAINER LanCache.Net Team <team@lancache.net>
 ARG DEBIAN_FRONTEND=noninteractive
 RUN \
   apt-get -y update && apt-get -y upgrade && \
-  apt-get -y install supervisor curl wget bzip2 locales tzdata && \
+  apt-get -y install python3-pip curl wget bzip2 locales tzdata && \
   locale-gen en_GB.utf8 && \
-  update-locale LANG=en_GB.utf8 && \
-  mkdir --mode 777 -p /var/log/supervisor && \
+  update-locale LANG=en_GB.utf8
+RUN \
+  pip3 install supervisor && \
+  mkdir --mode 777 -p /var/log/supervisor
+RUN \
   apt-get -y clean && \
   rm -rf /var/lib/apt/lists/*
 ENV \
@@ -14,7 +17,8 @@ ENV \
   LC_ALL=en_GB.UTF-8 \
   LANG=en_GB.UTF-8 \
   LANGUAGE=en_GB.UTF-8 \
-  TZ=Europe/London
+  TZ=Europe/London \
+  SUPERVISORD_LOGLEVEL=WARN
 COPY overlay/ /
 RUN chmod -R 755 /init /hooks
 ENTRYPOINT ["/bin/bash", "-e", "/init/entrypoint"]
--- run-tests.sh
+++ run-tests.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
-which goss
 
-if [ $? -ne 0 ]; then
-	echo "Please install goss from https://goss.rocks/install"
-	echo "For a quick auto install run the following"
-	echo "curl -fsSL https://goss.rocks/install | sh"
-	exit $?
+if [[ "$@" == *" -- "* ]]; then
+	SD_LOGLEVEL="-e SUPERVISORD_LOGLEVEL=INFO"
+else
+	SD_LOGLEVEL="-- -e SUPERVISORD_LOGLEVEL=INFO"
 fi
 
-docker build --tag lancachenet/ubuntu:goss-test .
-case $1 in
-  circleci)
-    shift;
-    mkdir -p ./reports/goss
-	if [[ "$1" == "keepimage" ]]; then
-		KEEPIMAGE=true
-		shift
-	fi
-    export GOSS_OPTS="$GOSS_OPTS --format junit"
-	dgoss run $@ lancachenet/ubuntu:goss-test > reports/goss/report.xml
-	#store result for exit code
-	RESULT=$?
-	#delete the junk that goss currently outputs :(
-    sed -i '0,/^</d' reports/goss/report.xml
-	#remove invalid system-err outputs from junit output so circleci can read it
-	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
-    ;;
-  *)
-	if [[ "$1" == "keepimage" ]]; then
-		KEEPIMAGE=true
-		shift
-	fi
-	dgoss run $@ lancachenet/ubuntu:goss-test
-	RESULT=$?
-    ;;
-esac
-[[ "$KEEPIMAGE" == "true" ]] || docker rmi lancachenet/ubuntu:goss-test
-
-exit $RESULT
+curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/ubuntu:goss-test" $@ $SD_LOGLEVEL
