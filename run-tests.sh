a77d6b91f3703b2740b61ff0e9d40c09be596c84
Amir Zarrinkafsh <nightah@me.com>
2021-10-16 20:27:21 +1100

    refactor: replace supervisord with s6
    This change looks to drop the python dependency in the base image and replaces supervisord with [s6](https://github.com/just-containers/s6-overlay).
    
    If we could get this branch pushed to the `s6` tag on Docker it will allow us to test this with the downstream containers.
    
    I will update the README.md if it's decided that we will adopt s6 over supervisord.

--- run-tests.sh
+++ run-tests.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-if [[ "$@" == *" -- "* ]]; then
-	SD_LOGLEVEL="-e SUPERVISORD_LOGLEVEL=INFO"
-else
-	SD_LOGLEVEL="-- -e SUPERVISORD_LOGLEVEL=INFO"
-fi
-
-curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/ubuntu:goss-test" $@ $SD_LOGLEVEL
+curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/ubuntu:goss-test" $@
